c9f18a0842393b0d61d66c334b3ef60f4e934ec3
j.ian <jay2u8809@gmail.com>
2023-04-15 14:26:52 +0900

    add plugin config interface

---
+++ src/lib/config/interface/config.interface.ts
@@ -0,0 +1,36 @@
+import { Custom } from '../../serverless/interface/serverless-config.interface';
+
+interface Stages {
+  [stage: string]: boolean;
+}
+
+export type DeployHistoryType = 'slack' | 'file';
+export type DeployHistorySettingType = SlackSetting | FileSetting;
+export type DeployHistory = Record<DeployHistoryType, DeployHistorySettingType>;
+interface DeployHistorySetting {
+  enable: boolean;
+}
+interface SlackSetting extends DeployHistorySetting {
+  // slack Token
+  token: string;
+
+  // slack channel name
+  channel: string;
+}
+
+export type FileType = 'csv' | 'tsv';
+interface FileSetting extends DeployHistorySetting {
+  type: FileType;
+  path: string;
+}
+
+export interface Config extends Custom {
+  // deploy history stages
+  stages: Stages;
+
+  // deploy history type
+  deployHistory: DeployHistory;
+
+  // UTC time zone - ex) JST: 9, EST: -5, ACST: 9.5
+  timezone?: number;
+}
